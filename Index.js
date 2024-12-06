3b9a2815008d5157d9d9c47042c74e78cc178e70
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2024-12-06 16:58:33 -0500

    idek cron foolery

--- Index.js
+++ Index.js
@@ -59,7 +59,10 @@ const sendTimeRemaining = async () => {
     (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
   );
 
-  const remainingMessage = `Time remaining until deadline: ${remainingDays} days, ${remainingHours} hours, and ${remainingMinutes} minutes.`;
+  const remainingMessage =
+    `content:
+          '# <a:dinkDonk:1303875324660158464> Secret Santa 2024 Is Live! <a:dinkDonk:1303875324660158464>\n## Closing date: 12/08 at 11:59PM EST\nUse the ` /
+    santa` command to join the list!\nWhen we reach the deadline the bot will DM each person their partner.\nGifts can be made/purchased from anywhere. If you get an international partner it may be cheaper to buy something from a company in their country instead of shipping one yourself.\n\n*Note: In the case that there is an uneven amount of santas then a random santa who agreed to do two people will be assigned the extra person.*'\mTime remaining until deadline: ${remainingDays} days, ${remainingHours} hours, and ${remainingMinutes} minutes.`;
 
   if (prevMsg) {
     await prevMsg.edit(remainingMessage);
